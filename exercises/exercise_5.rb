d18e3f88c12512dbdd26bb73655ce539fb01756b
Chris Mastel <chris.mastel@gmail.com>
2019-12-09 21:52:26 +0000

    determine store metrics (total rev, average rev, more than MM

--- exercises/exercise_5.rb
+++ exercises/exercise_5.rb
@@ -8,3 +8,19 @@ puts "Exercise 5"
 puts "----------"
 
 # Your code goes here ...
+
+### Exercise 5: Calculations
+
+# 1. Output the total revenue for the entire company (all stores), using Active Record's `.sum` calculation method.
+# 2. On the next line, also output the average annual revenue for all stores.
+# 3. Output the number of stores that are generating $1M or more in annual sales. **Hint:** Chain together `where` 
+#   and `size` (or `count`) Active Record methods.
+
+@total_revenue = Store.sum(:annual_revenue)
+puts "The total company revenue is: $#{@total_revenue}"
+
+@average_revenue = Store.average(:annual_revenue)
+puts "The average company revenue is: $#{@average_revenue}"
+
+@num_stores = Store.where("annual_revenue >= ?", 1000000).count()
+puts "There are #{@num_stores} that have more than $1000000 in annual revenue"
